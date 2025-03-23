7de623129ad216c1016b1d7f5b536969b18e00e2
shrihariatpepsales <shriharijadhav@pepsales.co>
2025-03-23 13:43:55 +0530

    Slack Landing page- Cactro

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import "../styles/header.css"
 
-import searchIcon from '../assets/icons/searchIcon.svg';
-import connectionIcon from '../assets/icons/connectionIcon.svg';
-import wifiIcon from '../assets/icons/wifiIcon.svg';
-import batteryIcon from '../assets/icons/batteryIcon.svg';
 import Navbar from './Navbar';
 
 const Header = () => {
